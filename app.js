229df15592d5cd97cbc8e9f39023adc1ee34ef6f
khe0124 <khe0124@gmail.com>
2020-03-19 23:58:40 +0900

    feature - graphql과 mongodb연동 1

--- app.js
+++ app.js
@@ -4,16 +4,33 @@ const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 
 const app = express();
+const events = [];
 
 app.use(bodyparser.json());
 app.use('/graphql', graphqlHttp({
     schema: buildSchema (
-        `type RootQuery {
-            events: [String!]!
+        `
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        type RootQuery {
+            events: [Event!]!
         }
 
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
 
         schema {
@@ -22,12 +39,19 @@ app.use('/graphql', graphqlHttp({
         }`
     ),
     rootValue: {
-        events: (args) => {
-            return ['Romantic Cooking', 'Sailing', 'All-night Party']
+        events: () => {
+            return events;
         },
         createEvent: (args) => {
-            const eventName = args.name;
-            return eventName;
+            const event = {
+              _id: Math.random().toString(),
+            title: args.eventInput.title,
+            description: args.eventInput.description,
+            price: args.eventInput.price,
+            date: args.eventInput.date
+            }
+            events.push(event);
+            return event;
         }
     },
     graphiql: true
